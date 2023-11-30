8aa1ae32f5e5ced574a92e9b805a752fe80f8e96
Wayu Tharai <iarahtuyaw@gmail.com>
2023-11-30 15:56:20 +0700

    Add Character and Mage

--- src/Thief.java
+++ src/Thief.java
@@ -1,42 +1,39 @@
-public class Mage extends Character{
-    private double matk;
-    public Mage(String name,Accessory accessory){
-        this.matk = 20;
+public class Thief extends Character{
+    public Thief(String name,Accessory accessory){
         this.name = name;
         this.runSpeed=10;
-        this.atk = 10;
-        this.def = 5;
+        this.atk = 20;
+        this.def = 10;
 //        if(accessory!=null){
 //            this.accessory  deep copy
-              //  buff
+        //  buff
 //        }
     }
-    public void magicAttack(Character op){
-        if(matk>op.def) {
-            double damage = matk-op.def;
-            System.out.println(this.name+" used magic attack to "+op.name+" "+damage +" damage ");
+    public void doubleAttack(Character op){
+        if(atk>op.def) {
+            double damage = (atk*2)-op.def;
+            System.out.println(this.name+" used double attack to "+op.name+" "+damage +" damage ");
             if(op.maxHp<=damage){
                 op.maxHp=0;
                 this.levelUp();
                 System.out.println(op.name+" is dead!");
             }else{
-                op.maxHp-=matk-op.def;
+                op.maxHp-=(atk*2)-op.def;
             }
         }else{
-            System.out.println(this.name+" used magic attack to "+op.name+ "0 damage ");
+            System.out.println(this.name+" used double attack to "+op.name+ "0 damage ");
         }
     };
     @Override
     protected void levelUp(){
-        System.out.println("You level up!");
+        System.out.println(name+" level up!");
 //        if(accessory!=null) accessory.levelUp();
         lvl++;
         maxHp=100+(10*lvl);
         maxMana=100+(10*lvl);
         runSpeed= 10 +(2*lvl);
-        matk = 20+(2*lvl);
-        atk = 10+(lvl);
-        def = 5+(lvl);
+        atk = 20+(2*lvl);
+        def = 10+(lvl);
 //        if(accessory!=null)  {
 //            this.runSpeed-=(accessory.lvl);
 //            //buff
