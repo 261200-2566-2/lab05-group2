815537f252047119f073b4dce2313f8eaa35b11e
Wayu Tharai <iarahtuyaw@gmail.com>
2023-12-05 16:07:54 +0700

    Change to interface

--- src/Accessory.java
+++ src/Accessory.java
@@ -1,33 +1,10 @@
-public class Accessory {
-    public String name;
-    protected double heavy;
-    protected  int lvl;
-    protected double runSpeed;
-    protected double matk;
-    public String type;
+public interface Accessory {
+    String name();
+    String type();
+    double getMatk();
 
+    double getHeavy();
 
-    protected Accessory(String name,int lvl,String type){
-        this.name=name;
-        this.type=type;
-        this.lvl=lvl;
-    }
-
-
-
-    public double getMatk(){
-        return matk;
-    }
-
-    public double getHeavy(){
-        return heavy;
-    }
-
-    public double getRunSpeed(){
-        return runSpeed;
-    }
-    public int getLvl(){
-        return lvl;
-    }
-
+    double getRunSpeed();
+    int getLvl();
 }
--- src/Character.java
+++ src/Character.java
@@ -1,92 +1,78 @@
 import java.util.Objects;
 
-public class Character {
-    public String name;
-    protected double maxHp = 100;
-    protected double maxMana = 50;
-    protected double runSpeed;
-    protected double atk;
-    protected double def;
-    protected double matk;
-    protected int lvl;
-    protected String classHero;
-
-    protected Accessory accessory;
-    protected Character(String name,Accessory accessory){
-        this.name = name;
-        this.runSpeed+=100;
-        if(accessory!=null){
-
-            if(Objects.equals(accessory.type, "Ring")){
-                this.accessory=new Ring(accessory.name,accessory.lvl);
-                this.matk=this.matk+this.accessory.getMatk();
-            }
-            if(Objects.equals(accessory.type, "Boots")){
-                this.accessory= new Boots(accessory.name,accessory.lvl);
-                this.runSpeed=this.runSpeed-this.accessory.getHeavy()+this.accessory.getRunSpeed();
-
-            }
-        }
-    }
-    public void attack(Character op){
-        if(atk>op.def) {
-            double damage = atk-op.def;
-            System.out.println(this.name+" attacked "+op.name+" "+damage +" damage ");
-            if(op.maxHp<=damage){
-                op.maxHp=0;
+public interface Character {
+    String name();
+    double maxHp();
+    void setMaxHp(double i);
+    double maxMana();
+    void setMaxMana(double i);
+    double runSpeed();
+    void setRunSpeed(double i);
+    double atk();
+    void setAtk(double i);
+    double def();
+    void setDef(double i);
+    double getMatk();
+    void setMatk(double i);
+    int getLvl();
+    void setLvl(int i);
+    String classHero();
+    Accessory accessory();
+    void setAccessory(Accessory i,String t);
+    default void attack(Character op){
+        if(atk()>op.def()) {
+            double damage = atk()-op.def();
+            System.out.println(this.name()+" attacked "+op.name()+" "+damage +" damage ");
+            if(op.maxHp()<=damage){
+                op.setMaxHp(0);
                 this.levelUp();
-                System.out.println(op.name+" is dead!");
+                System.out.println(op.name()+" is dead!");
             }else{
-                op.maxHp-=atk-op.def;
+                op.setMaxHp(op.maxHp()-(atk()-op.def()));
             }
         }else{
-            System.out.println(this.name+" attacked "+op.name+ " 0 damage ");
+            System.out.println(this.name()+" attacked "+op.name()+ " 0 damage ");
         }
     }
-    protected void levelUp(){
-        System.out.println(this.name+" level up!");
-        lvl++;
-        maxHp=100+(10*lvl);
-        maxMana=100+(10*lvl);
-        runSpeed= 10 +(2*lvl);
-        if(classHero.equals("Mage")){
-            matk = 20+(2*lvl);
-            atk = 10+(lvl);
-            def = 5+(lvl);
+    default void levelUp(){
+        System.out.println(this.name()+" level up!");
+        setLvl(this.getLvl()+1);
+        setMaxHp(100+(10*this.getLvl()));
+        setMaxMana(100+(10*getLvl()));
+        setRunSpeed(10 +(2*getLvl()));
+        if(classHero().equals("Mage")){
+            setMatk(20+(2*getLvl()));
+            setAtk(10+(getLvl()));
+            setDef(5+getLvl());
         }
-        if(classHero.equals("Thief")){
-            atk = 20+(2*lvl);
-            def = 10+(lvl);
+        if(classHero().equals("Thief")){
+            setAtk(20+(2*getLvl()));
+            setDef(10+getLvl());
         }
-        if(accessory!=null) {
-            if(Objects.equals(accessory.type, "Ring")){
-                this.accessory= new Ring(accessory.name,accessory.getLvl()+1);
-                this.matk+=accessory.getMatk();
+        if(accessory()!=null) {
+            if(Objects.equals(accessory().type(), "Ring")){
+                setAccessory(this.accessory(),"Ring");
+                setMatk(getMatk()+accessory().getMatk());
 
             }
-            if(Objects.equals(accessory.type, "Boots")){
-
-                this.accessory= new Boots(accessory.name,accessory.getLvl()+1);
-                this.runSpeed=this.runSpeed-this.accessory.getHeavy()+this.accessory.getRunSpeed();
-
-
+            if(Objects.equals(accessory().type(), "Boots")){
+                setAccessory(this.accessory(),"Boots");
+                setRunSpeed(runSpeed()- accessory().getHeavy() + accessory().getRunSpeed());
             }
 
         }
     }
-    public void show(){
-        System.out.println("Name :"+name);
-        System.out.println("Hp : "+this.maxHp);
-        System.out.println("Mana : "+this.maxMana);
-        System.out.println("damage : "+ this.atk);
-        System.out.println("def : "+ this.def);
-        System.out.println("magic attack : "+matk);
-        System.out.println("level : "+this.lvl);
-        System.out.println("runSpeed : "+this.runSpeed);
-        System.out.println("Class : "+this.classHero);
-        if(accessory==null) System.out.println("accessory : none");
-        else System.out.println("accessory : "+accessory.name);
-
+    default void show(){
+        System.out.println("Name :"+name());
+        System.out.println("Hp : "+this.maxHp());
+        System.out.println("Mana : "+this.maxMana());
+        System.out.println("damage : "+ this.atk());
+        System.out.println("def : "+ this.def());
+        System.out.println("magic attack : "+getMatk());
+        System.out.println("level : "+this.getLvl());
+        System.out.println("runSpeed : "+this.runSpeed());
+        System.out.println("Class : "+this.classHero());
+        if(accessory()==null) System.out.println("accessory : none");
+        else System.out.println("accessory : "+accessory().name());
     }
-
 }
